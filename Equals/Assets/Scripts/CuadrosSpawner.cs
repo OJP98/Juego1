7435746eb578a59789ab1f113d05851e69e587ba
josepablito16 <josepablocif16@gmail.com>
2018-06-05 17:10:06 -0600

    Idea 1

---
+++ Equals/Assets/Scripts/CuadrosSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuadrosSpawner : MonoBehaviour
+{
+    public GameObject cuadro;
+
+    public float spawnTime = 0.2f;
+    public float elapsedTime = 0f;
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (elapsedTime < spawnTime)
+        {
+            elapsedTime += Time.deltaTime / 10;
+        }
+        else
+        {
+
+            Instantiate(cuadro, new Vector3(-2.12f, 7.9f, 0f), Quaternion.identity);
+            Instantiate(cuadro, new Vector3(0f, 7.9f, 0f), Quaternion.identity);
+            Instantiate(cuadro, new Vector3(2.12f, 7.9f, 0f), Quaternion.identity);
+            elapsedTime = 0;
+
+        }
+
+    }
+}
